56152e8b1d9148648085accbefeb037ddd5d3513
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2021-07-01 16:43:13 +0100

    use regex
    replace the old password remover with regex

--- tools/password-remover/src/index.js
+++ tools/password-remover/src/index.js
@@ -6,11 +6,7 @@ const data = crypto.decodeCCFile('CCGameManager.dat');
 if (data.search('GJA_002') == -1)
     return console.log('[LOG] GJA_002 cannot be found')
 
-let password = data.search('GJA_002')
-password = data.slice(password, data.length)
-password = password.slice(14, password.search('</s>'))
-
-let build = data.replace(`<k>GJA_002</k><s>${password}</s>`, '<k>GJA_002</k><s>fakePassword</s>')
+let build = data.replace(/<k>GJA_002<\/k><s>(.*?)<\/s>/g, '<k>GJA_002</k><s></s>')
 
 
 
